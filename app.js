01b91e68bb4359b0c430ea79185b0970dd9ef15a
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2025-02-15 11:02:09 -0500

    Initial commit - Added project structure

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
+const tasksRoutes = require("./routes/tasks");
+
+const app = express();
+const PORT = 3000;
+
+// Set up Mustache as view engine
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./views");
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use("/", tasksRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
