77a555e6b6dc38a9d6ddb9d0c23eb56397266b37
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2025-02-16 17:41:42 -0500

    Data sorted by due date from oldest to newest by default

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         lengthChange: false,
         pageLength: 5, // Default rows per page
         ordering: true,
+        order:[[2,"asc"]],
         columnDefs: [{ targets: [5], orderable: false }], // Disable sorting on actions column
         language: {
             searchPlaceholder: "Search tasks...",
