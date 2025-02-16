60961a308730970356328d89d376366a4f372605
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2025-02-16 17:37:19 -0500

    Added red row for a table if due date is less than today's date

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,7 +8,11 @@ router.use(express.json());
 router.get("/", (req, res) => {
     db.all("SELECT * FROM tasks", (err, rows) => {
         if (err) return res.status(500).send("Error retrieving tasks");
-
+        const today = new Date().toISOString().split("T")[0];
+        
+        rows.forEach(task => {
+            task.isOverdue=task.due_date < today;
+        })
         rows = rows.map(task => ({ ...task, disabled: true }));
 
         res.render("index", { tasks: rows });
